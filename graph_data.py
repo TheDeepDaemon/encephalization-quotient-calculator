66b47c579f7f64cc0a951a5ac2e8274c55af4de1
TheDeepDaemon <evanpatterson@Evans-MacBook-Air.local>
2023-02-01 11:51:40 -0500

    added plotting

--- graph_data.py
+++ graph_data.py
@@ -1,5 +1,16 @@
-import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
+from keras.models import load_model
 
-def graph_model(model):
-    x = np.arange(1, 1000)
+def graph_model(model, max):
+    x = np.arange(1, max)
+    y = model.predict(x, verbose=False)
+    plt.plot(x,y, 'r')
+    plt.xlabel("body mass (grams)")
+    plt.ylabel("predicted brain mass (grams)")
+    plt.show()
+
+if __name__ == "__main__":
+    model_resource = '2x100-hidden-layers-model'
+    model = load_model(model_resource)
+    graph_model(model, 300000)
